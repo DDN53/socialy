393a8bbee71b5e2b514477328867c4df160aecf1
DDN53 <dinukanisheda9@gmail.com>
2025-03-23 20:48:36 +0530

    modify

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,22 @@
-
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
- import { currentUser } from "@clerk/nextjs/server";
+import { currentUser } from "@clerk/nextjs/server";
 import { syncUser } from "@/actions/user.action";
 
-export default async function Navbar() {
- const user = await currentUser();
-if (user) await syncUser(); // POST
+async function Navbar() {
+  const user = await currentUser();
+  if (user) await syncUser(); // POST
 
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <div className="flex items-center">
+          {/* <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
               Socially
             </Link>
-          </div>
+          </div> */}
 
           <DesktopNavbar />
           <MobileNavbar />
@@ -26,3 +25,4 @@ if (user) await syncUser(); // POST
     </nav>
   );
 }
+export default Navbar;
